6d8e7f8dd2e7ad46a356ac46e02d27e3ce58bae8
irezcen <83776275+irezcen@users.noreply.github.com>
2022-04-09 14:07:01 +0200

    files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zadanie_B3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            trójkątprostokątny trojkat1 = new trójkątprostokątny();
+            trójkątprostokątny trójkąt2 = new trójkątprostokątny();
+            trójkątprostokątny trójkąt3 = new trójkątprostokątny();
+            trojkat1.kolor = "czerwony";
+            trojkat1.A = 10;
+            trojkat1.B = 5;
+            trójkąt2.B = 3;
+            trójkąt2.A = 7.5;
+            trójkąt3.A = 7;
+            trójkąt3.B = 12.5;
+            trójkąt2.kolor = "maślany";
+            Console.WriteLine(trójkąt3.oblicztanges());
+            Console.WriteLine(trójkąt2.kolor);
+            Console.WriteLine(Math.Round(trojkat1.obwod, 2));
+        }
+    }
+}
---
+++ trójkątprostokątny.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadanie_B3
+{
+    class trójkątprostokątny
+    {
+        private double a;
+        private double b;
+        public string kolor
+        {
+            get;
+            set;
+        }
+        public double obwod
+        {
+            get
+            {
+                return A + B + obliczC();
+            }
+        }
+        public double A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    a = value;
+                }
+            }
+        }
+        public double B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    b = value;
+                }
+            }
+        }
+        public double oblicztanges()
+        {
+            return A / B;
+        }
+        private double obliczC()
+        {
+            return Math.Sqrt(Math.Pow(A, 2) + Math.Pow(B, 2));
+        }
+        public double OblicznSina()
+        {
+            return B / obliczC();
+        }      
+    }
+}
